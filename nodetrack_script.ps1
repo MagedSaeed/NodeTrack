f849d4db4f32287b5437625d4db99f3291e100e4
MagedSaeed <mageedsaeed1@gmail.com>
2025-02-24 13:00:34 +0300

    renaming

--- nodetrack_script.ps1
+++
@@ -1,2 +0,0 @@
-# Set-ExecutionPolicy ExecutionPolicy Bypass -Scope Process
-C:\Users\ondemand_admin\Desktop\Projects\NodeTrack\venv\Scripts\python.exe  C:\Users\ondemand_admin\Desktop\Projects\NodeTrack\run_script_as_service.py start client_daemon --script C:\Users\ondemand_admin\Desktop\Projects\NodeTrack\client\collect.py --log-dir C:\Users\ondemand_admin\Desktop\Projects\NodeTrack
