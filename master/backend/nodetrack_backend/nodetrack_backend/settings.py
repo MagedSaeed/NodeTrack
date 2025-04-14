0e8443c6c4135dec5102d68488ef8b6e7e50d1b4
MagedSaeed <mageedsaeed1@gmail.com>
2025-04-14 12:30:05 +0300

    set django debug as an env param

--- master/backend/nodetrack_backend/nodetrack_backend/settings.py
+++ master/backend/nodetrack_backend/nodetrack_backend/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG','False').capitalize() == 'True'
 
 ALLOWED_HOSTS = os.environ['DJANGO_ALLOWED_HOSTS'].split(',')
 
